7af3c88064d19e7b3a0f6fd196914e960089c37f
André Augusto Moreira Martins <64978311+AndreMartins21@users.noreply.github.com>
2022-10-01 15:39:06 -0300

    Update estimadores_reg_linear.R

--- REGRESSAO LINEAR/estimadores_reg_linear.R
+++ REGRESSAO LINEAR/estimadores_reg_linear.R
@@ -1,3 +1,15 @@
+"""
+# Observações:
+- A função abaixo `estimate_reg` estima os valores do intercepto (B0) e do 
+coeficiente angular (B1), para um modelo de Regressão Linear Simples. Isto, é, 
+estima os parâmetros de:
+                Y = B0 + B1x + E
+- Uma das maneiras de chegar nesses dois estimadores é através do método dos
+Mínimos Quadrados Ordinais (MQO), baseando-se na ideia de minimizar o erro (E), 
+através da derivada em relação a cada um desses parâmetros, em função do somatório
+do erro² (E²), igualando-a a 0.
+"""
+
 estimate_reg <- function(x, y){
   # Checks if x and y are the same length
   if(length(x) != length(y)){
@@ -21,6 +33,9 @@ estimate_reg <- function(x, y){
   return(c(b0, b1))
 }
 
+#-------------
+# Testes:
+#-------------
 x <- c(1.49, 1.41, 1.65, 1.03, 1.40, 1.10, 1.46, 1.35, 1.17, 0.99)
 y <- c(20.8, 18.0, 18.1, 6.0, 18.0, 8.0, 17.5, 12.6, 11.6, 9.2)
 
